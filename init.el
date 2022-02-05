b1cd887c92b261a1060f4111f53e0c1bc7da6fad
André Nogueira <andre@nog.dev>
2022-02-05 00:07:56 -0300

    changes mainly for windows: setting default font (also for standalone frames opened for emacs daemon); adding line numbers; setting default window size for gui; adding helper functions for reloading the init.el file and removing elpa directory then reloading file; adding quelpa for packages on github (alongside quelpa-use-package); tweaking evil settings a bit; changing default theme; adding modeline package; tweaking neotree settings; adding some QOL packages: good-scroll for windows, centaur-tabs; creating custom functions for various cases; adding keybindings to auto-open init.el amongst other reloading stuff; adding keybindings for some packages

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                          ;;
 ;; Setting up basic configs ;;
 ;;                          ;;
@@ -9,8 +9,13 @@
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (defalias 'yes-or-no-p 'y-or-n-p)
-(set-frame-font "JetBrains Mono-18" nil t)
+(set-frame-font "Cascadia Code-12" nil t)
+(setq default-frame-alist '((font . "Cascadia Code-18")))
+(global-display-line-numbers-mode t)
 (setq exec-path (append exec-path '("/Users/nog/.nvm/versions/node/v16.4.0/bin/")))
+(add-to-list 'default-frame-alist '(height . 28)) ;; Vertical frame size
+(add-to-list 'default-frame-alist '(width . 82)) ;; Horizontal frame size
+(require 'subr-x)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                            ;;
@@ -23,11 +28,12 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;                             ;;
-;; Bootstrapping "use-package" ;;
-;;                             ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                              ;;
+;; Bootstrapping "use-package"  ;;
+;; and quelpa (for github pkgs) ;;
+;;                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
@@ -37,6 +43,32 @@
   (setq use-package-always-ensure t
 	use-package-expand-minimally t))
 
+(defun nog-reload ()
+  "Tell emacs to read the init.el file again"
+  (interactive)
+  (load-file "~/.emacs.d/init.el")
+)
+
+(defun nog-packages-reinstall ()
+  "Remove elpa directory and tell emacs to read the init.el file again"
+  (interactive)
+  (delete-directory "~/.emacs.d/elpa" t)
+  (nog-reload)
+)
+
+
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
+
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://github.com/quelpa/quelpa-use-package.git"))
+(require 'quelpa-use-package)
+
 ;;;;;;;;;;;;;;
 ;;          ;;
 ;; Packages ;;
@@ -44,15 +76,18 @@
 ;;;;;;;;;;;;;;
 
 (use-package evil
+  :init
+  (setq evil-want-C-u-scroll t)
   :config
   (add-to-list 'evil-emacs-state-modes 'nav-mode)
   (evil-mode))
+
 (use-package doom-themes
   :config
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-vibrant t)
+  (load-theme 'doom-one-light t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
@@ -63,6 +98,9 @@
   (doom-themes-treemacs-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
 
 (use-package ivy
   :config
@@ -97,10 +135,10 @@
   :init
   (require 'neotree)
   :config
-  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (setq neo-theme 'arrow)
   (setq neo-smart-open t)
+  (setq neo-window-fixed-size nil)
 )
-(use-package all-the-icons)
 
 (use-package find-file-in-project)
 
@@ -122,6 +160,18 @@
     (add-hook 'before-save-hook 'tide-format-before-save)
     (add-hook 'typescript-mode-hook #'setup-tide-mode))
 )
+
+(use-package good-scroll
+  :quelpa (good-scroll :fetcher github :repo "io12/good-scroll.el")
+  :config
+  (good-scroll-mode 1)
+)
+
+(use-package centaur-tabs
+  :demand
+  :config
+  (centaur-tabs-mode t)
+  (setq centaur-tabs-set-icons t))
 
 ;;;;;;;;;;;;;;;;;;;;;;
 ;;                  ;;
@@ -153,13 +203,6 @@
       (setq nog-neotree-open nil)))
   )
 
-(defun nog-reload-packages ()
-  "Remove elpa directory and tell emacs to read the init.el file again"
-  (interactive)
-  (delete-directory "~/.emacs.d/elpa" t)
-  (load-file "~/.emacs.d/init.el")
-  )
-
 (defun setup-tide-mode ()
     (interactive)
     (tide-setup)
@@ -172,6 +215,41 @@
     ;; `M-x package-install [ret] company`
     (company-mode +1))
 
+(defun nog-windows-path (path)
+  "Format unix path so windows can understand it"
+  (interactive)
+  (replace-regexp-in-string "/" "\\\\" path)
+)
+
+(defun nog-get-filename-format (filename)
+  "Split string by dot (.) and get last element of the list"
+  (string-trim (car (last (split-string filename "\\.")))))
+
+(defun nog-insert-shell-command-output (command prefix)
+  "Execute shell command, get result, move to end of line,
+  insert 2 newlines with the result of the command, prefixed if
+  prefix was given"
+  (interactive)
+  (end-of-line)
+  (newline-and-indent)
+  (newline-and-indent)
+  (if prefix
+      (insert (concat prefix (shell-command-to-string command)))
+    (insert (shell-command-to-string command))))
+
+(defun nog-run-file-through-command ()
+  "Get current filename format, based on that format choose which command to run
+  (output will be inserted 2 lines below, with provided prefix, if non-nil)"
+  (interactive)
+  (let (
+	(format (nog-get-filename-format (buffer-file-name)))
+       )
+    (cond
+     ((equal format "lisp") (nog-insert-shell-command-output (concat "sbcl --script " (buffer-file-name)) ";; "))
+     (t (message (concat "Can't run program for current file! Unknown format: " format)))
+    )
+  )
+)
 
 ;;;;;;;;;;;;;;;;;
 ;;             ;;
@@ -180,6 +258,16 @@
 ;;;;;;;;;;;;;;;;;
 
 (global-set-key [f8] 'neotree-project-dir)
+(global-set-key [f12] (lambda ()
+			(interactive)
+			(find-file "~/.emacs.d/init.el")))
+(global-set-key [f7] 'nog-run-file-through-command)
+(global-set-key (kbd "C-c r") (lambda ()
+				(interactive)
+				(nog-reload)))
+(global-set-key (kbd "C-c C-r") (lambda ()
+				  (interactive)
+				  (nog-packages-reinstall)))
 (global-set-key (kbd "C-c q") (lambda ()
 				(interactive)
 				(other-window -1)))
@@ -196,9 +284,16 @@
 (evil-define-key 'normal neotree-mode-map (kbd "A") 'neotree-stretch-toggle)
 (evil-define-key 'normal neotree-mode-map (kbd "H") 'neotree-hidden-file-toggle)
 
+(define-key evil-normal-state-map (kbd "g t") 'centaur-tabs-forward)
+(define-key evil-normal-state-map (kbd "g T") 'centaur-tabs-backward)
+
+(global-set-key [next] #'good-scroll-up-full-screen)
+(global-set-key [prior] #'good-scroll-down-full-screen)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;
 ;; init.el ends here! ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -206,7 +301,7 @@
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
  '(package-selected-packages
-   '(doom-themes one-themes tide find-file-in-project all-the-icons flycheck-rust cargo racer rust-mode neotree smartparens which-key flycheck company ivy evil use-package)))
+   '(slime centaur-tabs good-scroll doom-modeline quelpa-use-package quelpa doom-themes one-themes tide find-file-in-project all-the-icons flycheck-rust cargo racer rust-mode neotree smartparens which-key flycheck company ivy evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
